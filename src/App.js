98e37893dad1d218f7a27588dbfcdb1b307c475b
Tan Nguyen <tannguyen85@gmail.com>
2021-04-23 07:46:48 +0200

    creating project, adding first page and navbar,switch,route, install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import NavBar from "./Components/Navbar/navbar";
+import MainPage from "./Components/MainPage/MainPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function BasicExample() {
+    return (
+        <Router>
+            <div>
+                <NavBar/>
+                <Switch>
+                    <Route exact path="/">
+                        <MainPage />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
+}
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+        </div>
+    );
+}
---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MainPage() {
+    return(
+        <div>
+          <h1>Welcome to the Officedays booking tool</h1>
+          <h2>Through this website you can reserve a desk at your office. Please first select the office you want to book a desk at.</h2>
+          <h3>Amsterdam Office</h3>
+          <h3>Utrecht Office</h3>
+        </div>
+    )
+}
+
+export default MainPage
